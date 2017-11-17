e5689d9907e7308c415a9df7aac080e5300f3afa
Jed <jed.reisner@gmail.com>
2017-11-16 17:15:48 -0700

    Added tests.

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -1,9 +1,14 @@
 package page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by admin on 11/15/2017.
@@ -18,4 +23,22 @@ public class HomePage {
 
     @FindBy(className = "continueButton")
     WebElement continueButton;
+
+    @FindBy(className = "checkbox")
+    WebElement agreeCheckbox;
+
+
+
+
+    public void clickContinue(){
+        driver.navigate().to("https://www.nuskin.com/content/nuskin/en_US/ageloc-me-assessment.html#/you-start");
+        WebDriverWait wait = new WebDriverWait(driver, 15, 100);
+        WebElement loginLightbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkbox")));
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(agreeCheckbox).click().perform();
+
+
+        continueButton.click();
+    }
 }
--- src/main/java/test/Tests.java
+++ src/main/java/test/Tests.java
@@ -9,11 +9,12 @@ import static org.testng.Assert.assertEquals;
 /**
  * Created by admin on 11/15/2017.
  */
-public class Test extends TestBase {
+public class Tests extends TestBase {
 
     @Test
     public void titleTest(){
         HomePage homePage = new HomePage(driver);
+        homePage.clickContinue();
         //System.out.println(homePage.getTitle());
         assertEquals( driver.getTitle(), "Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah");
     }
