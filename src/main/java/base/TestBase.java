6bba60981fb494100e7498aa9078a5afe3cd429c
Jed Reisner <djechotech@gmail.com>
2017-12-15 15:32:08 -0700

    Mobile emulation testing

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -8,6 +8,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,10 +23,19 @@ public class TestBase {
         System.setProperty("webdriver.chrome.driver", "C:\\\\chromedriver\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-geolocation");
+
+        //Mobile Emulation
+        /*Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "Nexus 5");
+        options.setExperimentalOption("mobileEmulation", mobileEmulation);*/
+
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
         }
+
 
     @AfterTest
     public void tearDown() throws InterruptedException {
