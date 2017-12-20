e977e34143c760e96d0675bef91c73963bbbc28c
Jed Reisner <djechotech@gmail.com>
2017-12-20 13:53:58 -0700

    Made mobile emulation so it has to be set manually for now.

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -19,34 +19,37 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestBase {
     protected static WebDriver driver;
+    public boolean mobileTest;
 
     @BeforeTest
     public void init() {
         System.setProperty("webdriver.chrome.driver", "C:\\\\chromedriver\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-geolocation");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-    }
+        mobileTest=true;
 
-    public void mobileEmu(String device){
-        String deviceName = device;
+        //Mobile Emulation
+        String deviceName = "Galaxy S5";
         Map<String, String> devices =  new HashMap<>();
         devices.put("deviceName", deviceName);
         Map<String, Object> mobileEmulation = new HashMap<>();
         mobileEmulation.put("mobileEmulation", devices);
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(ChromeOptions.CAPABILITY, mobileEmulation);
-        driver = new ChromeDriver(capabilities);
-        System.out.println("Mobile emulation testing for "+deviceName);
-    }
 
+        if(mobileTest){
+            driver = new ChromeDriver(capabilities);
+            System.out.println("Mobile emulation testing for "+deviceName);
+        } else {
+            driver = new ChromeDriver(options);
+        }
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
 
     @AfterTest
     public void tearDown() throws InterruptedException {
         Thread.sleep(1000);
-
         //Close all tabs
         for(String handle : driver.getWindowHandles()) {
             driver.switchTo().window(handle);
--- src/main/java/test/Tests.java
+++ src/main/java/test/Tests.java
@@ -37,61 +37,6 @@ public class Tests extends TestBase {
         homePage.finishAssessment();
         //System.out.println(homePage.getTitle());
         assertEquals( homePage.getCareCode(), "AB73");
+
     }
-
-    @Test(priority=1)
-    public void setupGalaxyTest() throws Exception{
-        HomePage homePage = new HomePage(driver);
-        homePage.startAssessment("https://www.nuskin.com/content/nuskin/en_US/ageloc-me-assessment.html#/you-start");
-        homePage.enterName("Jed", "32", "male");
-        homePage.enterEthnicity("caucasian");
-        homePage.enterCity("Salt Lake City");
-        homePage.chemExposure(77);
-        homePage.sunExposure(50);
-        homePage.setSkinType("dry");
-        homePage.setSkinIrritability(5);
-        homePage.setAhaUse("stopped");
-        homePage.setAgeSpots(20);
-        homePage.setWrinkles(30,45,55);
-        homePage.setPoreSize(40);
-        homePage.setFirmness("veryFirm");
-        homePage.setRadiance("veryRadiant");
-        homePage.setTexture("rough");
-        homePage.setFragrance("yes");
-        homePage.setDayMoisturizer("");
-        homePage.setFragrance("no");
-        homePage.setNightMoisturizer("20");
-        homePage.finishAssessment();
-        //System.out.println(homePage.getTitle());
-        assertEquals( homePage.getCareCode(), "AB73");
-    }
-
-    @Test(priority=2)
-    public void setupIpadTest() throws Exception{
-        HomePage homePage = new HomePage(driver);
-        homePage.startAssessment("https://www.nuskin.com/content/nuskin/en_US/ageloc-me-assessment.html#/you-start");
-        homePage.enterName("Jed", "32", "male");
-        homePage.enterEthnicity("caucasian");
-        homePage.enterCity("Salt Lake City");
-        homePage.chemExposure(77);
-        homePage.sunExposure(50);
-        homePage.setSkinType("dry");
-        homePage.setSkinIrritability(5);
-        homePage.setAhaUse("stopped");
-        homePage.setAgeSpots(20);
-        homePage.setWrinkles(30,45,55);
-        homePage.setPoreSize(40);
-        homePage.setFirmness("veryFirm");
-        homePage.setRadiance("veryRadiant");
-        homePage.setTexture("rough");
-        homePage.setFragrance("yes");
-        homePage.setDayMoisturizer("");
-        homePage.setFragrance("no");
-        homePage.setNightMoisturizer("20");
-        homePage.finishAssessment();
-        //System.out.println(homePage.getTitle());
-        assertEquals( homePage.getCareCode(), "AB73");
-    }
-
-
 }
