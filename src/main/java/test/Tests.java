3e48b69360760c842c3098370b187e6adce71f42
Jed Reisner <djechotech@gmail.com>
2018-01-19 13:04:21 -0700

    Enabled parallel testing mode.

--- src/main/java/test/Tests.java
+++ src/main/java/test/Tests.java
@@ -20,7 +20,7 @@ import static org.testng.Assert.assertEquals;
  */
 public class Tests extends TestBase {
 
-    @DataProvider(name="userData")
+    @DataProvider(name="userData", parallel = true)
     public Object[][] createData1() throws Exception{
         Object[][] retObjArr=getTableArray("C:\\\\Users\\\\Jed Reisner\\\\IdeaProjects\\\\Hackathon2017\\\\src\\\\main\\\\java\\\\testdata\\\\assessmentdata.xls",
                 "Sheet1");
@@ -88,7 +88,7 @@ public class Tests extends TestBase {
         Log.endTestCase("setupTest");
     }
 
-    @Test(dataProvider="userData")
+    @Test(dataProvider="userData", threadPoolSize = 3, invocationCount = 1)
     public void setupTestExcel(String CountryURL, String Country, String Age, String Gender, String Race, String City, String ChemExp, String SunExp, String SkinType,
     String Irritability, String Aha, String AgeSpots, String Wrinkles, String Nose, String Pores, String PoreSize, String Firmness, String Radiant, String Texture,
     String DayFrag, String DayMoist, String NightFrag, String NightMoist, String OrigAssessment) throws Exception{
